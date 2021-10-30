3fbc547081fa36dcbff17b18062542307b68e316
giuseppeps <54049840+giuseppeps@users.noreply.github.com>
2021-10-30 15:40:07 -0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -65,9 +65,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "dc5eaa24790ebe0ddca77939ab2deee4",
-"/": "dc5eaa24790ebe0ddca77939ab2deee4",
-"main.dart.js": "9d60ea00717e729474a8b602d7d7c532",
+"index.html": "63868ce87b0aa8189754ba11feebe8d8",
+"/": "63868ce87b0aa8189754ba11feebe8d8",
+"main.dart.js": "8a3f123547b23d6aa115b1d9f719f04b",
 "manifest.json": "d358fa928db16344efcbb02c51775fce",
 "version.json": "a6a60e5a2634705e6693aa6be9d42224"
 };
